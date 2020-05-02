c946ae35746ef8af718b5bbc17d7c60b818508af
capricho13 <rickywinata1st@gmail.com>
2020-05-02 18:08:55 +0700

    Add feature1

--- app.js
+++ app.js
@@ -2,3 +2,4 @@
 console.log("Hello World Ricky!\n");
 console.log("Print any text required\n");
 console.log("I need this comment to be pushed to remote repository!");
+console.log("Currently on feature1 branch");
